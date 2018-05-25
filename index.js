24e65a894dfe83f6853be947de2ea4d94c58bcdf
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-25 19:25:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ bot.on("ready", async () => {
 });
 
 bot.on("message", async message => {
+	
 
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
@@ -14,18 +15,34 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
+  
 
-  if (cmd === `!commands`){
-    message.channel.send("```md\n# BOT commands: \n\n• !play (Use this command to play MiniPólis) \n• !info (BOT information) \n• Command 3 (Coming soon) \n```");
-  }
+if (cmd === `!info`){
+  message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: bot.user.username,
+      icon_url: bot.user.avatarURL
+    },
+    title: "BOT information:",
+    url: "https://www.facebook.com/Will.MiniPolis",
+    fields: [{
+        name: "Developer:                                                                           Version:",
+        value: "Will Ajudante-MP                                             1.0.0"
+      },
+      {
+        name: "Update:                                                                             Name of BOT:",
+        value: "25/05/2018                                                   Moderator"
+      },
+      {
+        name: "Language:                                                                                                   Owner:",
+        value: "English                                            [www.MiniPolis.com.br](https://www.minipolis.com.br/)"
+      },
+    ],
+  }})
+}
 
-  if (cmd === `!play`){
-    message.channel.send("```md\n# Play MiniPólis: \n\nTo test the ALPHA version of the MiniPólis just go to our website: \n• ALPHA.MiniPolis.com.br \n```");
-  }
 
-  if (cmd === `!infa`){
-    message.channel.send("```md\n# BOT - MiniPólis: \n\n• Developer: Will Ajudante-MP \n• Version: 1.0.0 \n• Update: 25/05/2018 \n• Name of BOT: Moderator \n• Language: English \n• Owner: MiniPolis.com.br \n```");
-  }
 });
 
 bot.login(process.env.TOKEN);
