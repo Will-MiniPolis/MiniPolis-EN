f18af7fcc4066304e86144d9852934a6f881add4
Will-MiniPolis <39627424+Will-MiniPolis@users.noreply.github.com>
2018-05-25 19:28:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (cmd === `!info`){
     title: "BOT information:",
     url: "https://www.facebook.com/Will.MiniPolis",
     fields: [{
-        name: "Developer:                                                                                                Version:",
+        name: "Developer:                                                                                                        Version:",
         value: "Will Ajudante-MP                                             1.0.0"
       },
       {
@@ -35,7 +35,7 @@ if (cmd === `!info`){
         value: "25/05/2018                                                   Moderator"
       },
       {
-        name: "Language:                                                                                                   Owner:",
+        name: "Language:                                                                                                            Owner:",
         value: "English                                                        [www.MiniPolis.com.br](https://www.minipolis.com.br/)"
       },
     ],
